f8d8794eb4039947ed10b2e82d426f2633d38b0f
Dev-C137 <dev.91096@gmail.com>
2018-07-05 21:04:24 +0530

    statred networking stuff

--- main.py
+++ main.py
@@ -1,34 +1,7 @@
-import requests
 import string
 import random
 from pprint import pprint
-
-
-class hq:
-
-    def verify(phone: str) -> str:
-        try:
-            return requests.post("https://api-quiz.hype.space/verifications", data={
-                "method": "sms",
-                "phone": "+1" + phone
-            }).json()["verificationId"]
-        except KeyError:
-            raise Exception("invalid phone number")
-
-    def submit_code(verification_id: str, code: str) -> dict:
-        return requests.post("https://api-quiz.hype.space/verifications/" + verification_id, data={"code": code}).json()
-
-    def username_available(username: str) -> bool:
-        return not bool(requests.post("https://api-quiz.hype.space/usernames/available", data={"username": username}).json())
-
-    def create_user(username: str, verification_id: str, referral: str="", region: str="US", language: str="en"):
-        return requests.post("https://api-quiz.hype.space/users", data={
-            "country": region,
-            "language": language,
-            "referringUsername": referral,
-            "username": username,
-            "verificationId": verification_id
-        }).json()
+import pyhq as hq
 
 
 if __name__ == '__main__':
---
+++ te.py
@@ -0,0 +1,24 @@
+from pyhq import HQClient
+from pprint import pprint
+import requests
+
+
+auth_token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjIxMjQ3MDczLCJ1c2VybmFtZSI6ImRldjAwNDM1NSIsImF2YXRhclVybCI6Imh0dHBzOi8vZDJ4dTFoZG9taDNucnguY2xvdWRmcm9udC5uZXQvZGVmYXVsdF9hdmF0YXJzL1VudGl0bGVkLTFfMDAwNF9nb2xkLnBuZyIsInRva2VuIjpudWxsLCJyb2xlcyI6W10sImNsaWVudCI6IiIsImd1ZXN0SWQiOm51bGwsInYiOjEsImlhdCI6MTUzMDI1NDU0OSwiZXhwIjoxNTM4MDMwNTQ5LCJpc3MiOiJoeXBlcXVpei8xIn0.TF7IS2r6laPVwTkfb5eUkkmuf7YR5iu-QmHsdUYJ1Io'
+
+
+def default_headers():
+    return {
+        "x-hq-client": "Android/1.7.2",
+        "authorization": "Bearer " + auth_token,
+        "user-agent": "okhttp/3.8.0"
+    }
+
+
+def search_users(user: str):
+    response = requests.get("https://api-quiz.hype.space/users?q=" + user, headers=default_headers(), verify=True).json()
+    return response
+
+
+res = search_users("deivu")
+
+pprint(res)
