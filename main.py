11e35f1fe472d1d7ae631d8cde3dd844e3a119a6
Dev-C137 <dev.91096@gmail.com>
2018-07-08 14:45:53 +0530

    Store accounts in text file

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from pprint import pprint
 import pyhq as hq
 import networking as n
 import asyncio
+import requests
 
 
 def generate_account():
@@ -14,36 +15,94 @@ def generate_account():
     otp = input("OTP : ")
     sub_code_res = hq.submit_code(verification_id, otp)
 
-    refferal_code = input("Refferal : ")
+    if sub_code_res['auth'] is not None:
+        print("Sorry, this number is associated with {}".format(sub_code_res['auth']['username']))
+        return '', '', ''
+
+    referral_code = input("Referral : ")
 
     username = ''.join(random.choice(string.ascii_lowercase) for _ in range(6)) + ''.join(random.choice(string.digits) for _ in range(3))
 
     if hq.username_available(username):
-        res = hq.create_user(username, verification_id, refferal_code)
+        res = hq.create_user(username, verification_id, referral_code)
     else:
         print("Try Again")
+        return '', '', ''
 
-    pprint(res)
-
-    BEARER_TOKEN = res['authToken']
+    auth_token = res['authToken']
 
     # BEARER_TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjIxNDc3MzU5LCJ1c2VybmFtZSI6Imh5ZWFsdDk0MSIsImF2YXRhclVybCI6Imh0dHBzOi8vZDJ4dTFoZG9taDNucnguY2xvdWRmcm9udC5uZXQvZGVmYXVsdF9hdmF0YXJzL1VudGl0bGVkLTFfMDAwM19yZWQucG5nIiwidG9rZW4iOm51bGwsInJvbGVzIjpbXSwiY2xpZW50IjoiIiwiZ3Vlc3RJZCI6bnVsbCwidiI6MSwiaWF0IjoxNTMwODM3NTg5LCJleHAiOjE1Mzg2MTM1ODksImlzcyI6Imh5cGVxdWl6LzEifQ.-8Giv3aTj0g-zItZb5lzlUt_hA1tRqcQLoi9NMrhvFU'
+    # BEARER_TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjIxNTI0NTg5LCJ1c2VybmFtZSI6InRzZHVvZzA2NCIsImF2YXRhclVybCI6Imh0dHBzOi8vZDJ4dTFoZG9taDNucnguY2xvdWRmcm9udC5uZXQvZGVmYXVsdF9hdmF0YXJzL1VudGl0bGVkLTFfMDAwMF9ncmVlbi5wbmciLCJ0b2tlbiI6bnVsbCwicm9sZXMiOltdLCJjbGllbnQiOiIiLCJndWVzdElkIjpudWxsLCJ2IjoxLCJpYXQiOjE1MzA5NzgxMzQsImV4cCI6MTUzODc1NDEzNCwiaXNzIjoiaHlwZXF1aXovMSJ9.dPK182mjbblwHpw0oz7RCxjai54Y4nvQdIwchOcr-rE'
+    # BEARER_TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjIxNTQ2NTE1LCJ1c2VybmFtZSI6InF0b2N6cjY1MiIsImF2YXRhclVybCI6Imh0dHBzOi8vZDJ4dTFoZG9taDNucnguY2xvdWRmcm9udC5uZXQvZGVmYXVsdF9hdmF0YXJzL1VudGl0bGVkLTFfMDAwM19yZWQucG5nIiwidG9rZW4iOm51bGwsInJvbGVzIjpbXSwiY2xpZW50IjoiIiwiZ3Vlc3RJZCI6bnVsbCwidiI6MSwiaWF0IjoxNTMxMDM3MzczLCJleHAiOjE1Mzg4MTMzNzMsImlzcyI6Imh5cGVxdWl6LzEifQ.GOq9aqqgEWpDr2Q3tEIryQgf5s8x2_AWfwCu9NvOb9Y'
 
-    return BEARER_TOKEN
+    return username, auth_token, referral_code
 
 
-def stimulate_play_game(BEARER_TOKEN):
+def write_data(u, a, r):
+
+    file = open('data.txt', 'a')
+    file.write('{} {} {}\n'.format(u, a, r))
+    file.close()
+
+
+def show_active():
+
+    url = 'https://api-quiz.hype.space/shows/now'
+    response = requests.get(url).json()
+    return response['active']
+
+
+def get_socket_url(token):
 
     main_url = f"https://api-quiz.hype.space/shows/now?type="
-    headers = {"Authorization": f"Bearer {BEARER_TOKEN}", "x-hq-client": "Android/1.3.0"}
+    headers = {"Authorization": f"Bearer {token}", "x-hq-client": "Android/1.3.0"}
 
     response_data = asyncio.get_event_loop().run_until_complete(n.get_json_response(main_url, timeout=1.5, headers=headers))
 
     pprint(response_data)
 
+    socket_url = response_data['broadcast']['socketUrl'].replace("https", "wss")
+
+    return socket_url
+
+def read_data():
+
+    file =
+
+
+# def stimulate_play_game(BEARER_TOKEN):
+
 
 if __name__ == '__main__':
 
-    B = generate_account()
+    prompt = '\n> '
+    print('Create Account - c \nStimulate Play Game - s ')
+    op = input(prompt)
 
-    stimulate_play_game(B)
+    while op != 'q':
+
+        if op == 'c':
+
+            username, token, referral = generate_account()
+
+            if username != '':
+                write_data(username, token, referral)
+                print('Account with name {} created successfully!'.format(username))
+
+        elif op == 's':
+
+            if not show_active():
+                print('The show not active, Try again!')
+                continue
+
+
+
+        else:
+            print('Wrong option try again')
+
+        op = input(prompt)
+
+
+
+
+
--- te.py
+++ te.py
@@ -1,9 +1,9 @@
-from pyhq import HQClient
+import pyhq as hq
 from pprint import pprint
 import requests
 
 
-auth_token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjIxMjQ3MDczLCJ1c2VybmFtZSI6ImRldjAwNDM1NSIsImF2YXRhclVybCI6Imh0dHBzOi8vZDJ4dTFoZG9taDNucnguY2xvdWRmcm9udC5uZXQvZGVmYXVsdF9hdmF0YXJzL1VudGl0bGVkLTFfMDAwNF9nb2xkLnBuZyIsInRva2VuIjpudWxsLCJyb2xlcyI6W10sImNsaWVudCI6IiIsImd1ZXN0SWQiOm51bGwsInYiOjEsImlhdCI6MTUzMDI1NDU0OSwiZXhwIjoxNTM4MDMwNTQ5LCJpc3MiOiJoeXBlcXVpei8xIn0.TF7IS2r6laPVwTkfb5eUkkmuf7YR5iu-QmHsdUYJ1Io'
+# auth_token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjIxMjQ3MDczLCJ1c2VybmFtZSI6ImRldjAwNDM1NSIsImF2YXRhclVybCI6Imh0dHBzOi8vZDJ4dTFoZG9taDNucnguY2xvdWRmcm9udC5uZXQvZGVmYXVsdF9hdmF0YXJzL1VudGl0bGVkLTFfMDAwNF9nb2xkLnBuZyIsInRva2VuIjpudWxsLCJyb2xlcyI6W10sImNsaWVudCI6IiIsImd1ZXN0SWQiOm51bGwsInYiOjEsImlhdCI6MTUzMDI1NDU0OSwiZXhwIjoxNTM4MDMwNTQ5LCJpc3MiOiJoeXBlcXVpei8xIn0.TF7IS2r6laPVwTkfb5eUkkmuf7YR5iu-QmHsdUYJ1Io'
 
 
 def default_headers():
@@ -19,6 +19,11 @@ def search_users(user: str):
     return response
 
 
-res = search_users("deivu")
+if __name__ == '__main__':
 
-pprint(res)
+    phone_number = input('Phone Number : ')
+    verification_id = hq.verify(phone_number)
+    otp = input("OTP : ")
+    sub_code_res = hq.submit_code(verification_id, otp)
+
+    pprint(sub_code_res)
